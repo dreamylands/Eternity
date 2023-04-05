4e585d974d1b64615ab4ab0444be4213f0673995
dreamylands <playboyhein77@gmail.com>
2023-04-05 13:28:25 +0900

    add database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const discord = require('discord.js')
 require("dotenv").config({debug : true})
+const mongoose = require("mongoose")
 const client = new discord.Client({
     intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMessages , discord.GatewayIntentBits.MessageContent]
 })
 const token = process.env.TOKEN
+const db = process.env.DB
 client.commands = new discord.Collection()
 client.events = new discord.Collection()
 const namearray = ['commandhandler' , 'eventhandler']
@@ -14,6 +16,16 @@ client.once("ready" , async() => {
 
         client.user.setActivity('Over Elysian' , {type : "WATCHING"})
         console.log ("Eternity is online")
+        await mongoose.connect(db , {
+            useNewUrlParser : true ,
+            useUnifiedTopology : true ,
+
+        }).then (() => {
+            console.log("connected to database")
+        }).catch((err) => {
+            console.log(err)
+            mongoose.connection.close()
+        })
 
 
 })
